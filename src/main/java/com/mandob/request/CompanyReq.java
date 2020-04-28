493985259da10aa112ade3f4c74256be6b796198
Mohamed Essam <mohamed.essam.mohamed.kamel@gmail.com>
2020-04-28 14:18:07 +0200

    edit company service and fix bugs

--- src/main/java/com/mandob/request/CompanyReq.java
+++ src/main/java/com/mandob/request/CompanyReq.java
@@ -12,7 +12,6 @@ public class CompanyReq {
     @NotBlank(message = "must-not-be-blank")
     private String enName;
 
-    @NotBlank(message = "must-not-be-blank")
     private Boolean suspended;
 
     @NotBlank(message = "must-not-be-blank")
